f71ee1cdc53d6beaf84f9fe38ac65a4a0a477829
Xavier Villaneau <xvillaneau@gmail.com>
2019-12-22 21:56:07 -0500

    Solved AoC 2019 day 22

---
+++ aoc_2019/day_22.py
@@ -0,0 +1,82 @@
+from itertools import chain, compress
+import re
+import numpy as np
+
+from libaoc.files import read_lines
+from libaoc.primes import extended_euclidian_algorithm
+
+def deal_new_stack(deck):
+    return deck[::-1]
+
+def cut_stack(deck, n):
+    return np.roll(deck, -n)
+
+def deal_with_increment(deck, n):
+    r = len(deck)
+    ind = np.arange(r)
+    rev_ind = (ind * n) % r
+    np.put(ind, rev_ind, np.arange(r))
+    return deck[ind]
+
+def parse_techniques(techniques):
+    commands = []
+    for line in techniques:
+        if line == "deal into new stack":
+            commands.append((0, 0))
+        elif m := re.match(r"cut (-?\d+)", line):
+            commands.append((1, int(m.group(1))))
+        elif m := re.match(r"deal with increment (\d+)", line):
+            commands.append((2, int(m.group(1))))
+        else:
+            raise ValueError(f"Bad line: {line}")
+    return commands
+
+def follow_card(commands, card: int, deck_size):
+    position = card
+    for cmd, arg in commands:
+        if cmd == 0:
+            position = deck_size - position - 1
+        elif cmd == 1:
+            position = (position - arg) % deck_size
+        else:  # incr
+            position = (position * arg) % deck_size
+    return position
+
+def revert_card(commands, position: int, deck_size):
+    for cmd, arg in commands[::-1]:
+        if cmd == 0:
+            position = deck_size - position - 1
+        elif cmd == 1:
+            position = (position + arg) % deck_size
+        else:  # incr
+            p, _, _ = extended_euclidian_algorithm(arg, deck_size)
+            position = (p * position) % deck_size
+    return position
+
+def merge_commands(commands, deck_size):
+    zero = follow_card(commands, 0, deck_size)
+    one = follow_card(commands, 1, deck_size)
+    offset = deck_size - zero
+    increment = (one - zero) % deck_size
+    return [(2, increment), (1, offset)]
+
+def merge_commands_n_times(commands, deck_size, n_steps: int):
+    components = [merge_commands(commands, deck_size)]
+    while len(components) < n_steps.bit_length():
+        components.append(merge_commands(components[-1] + components[-1], deck_size))
+    bits = [b == "1" for b in bin(n_steps)[:1:-1]]
+    sub_cmds = list(chain.from_iterable(compress(components, bits)))
+    return merge_commands(sub_cmds, deck_size)
+
+def day_22(techniques):
+    commands = parse_techniques(techniques)
+
+    yield follow_card(commands, 2019, 10007)
+
+    deck_size, n_steps = 119_315_717_514_047, 101_741_582_076_661
+    merged_commands = merge_commands_n_times(commands, deck_size, n_steps)
+    yield revert_card(merged_commands, 2020, deck_size)
+
+if __name__ == '__main__':
+    from libaoc import iter_main
+    iter_main(2019, 22, read_lines, day_22)
---
+++ tests_2019/test_2019_22.py
@@ -0,0 +1,89 @@
+import numpy as np
+import pytest
+from aoc_2019.day_22 import (
+    deal_with_increment,
+    deal_new_stack,
+    cut_stack,
+    follow_card,
+    parse_techniques,
+    merge_commands,
+    merge_commands_n_times,
+    revert_card
+)
+
+def test_deal_new_stack():
+    deck = np.arange(10)
+    assert list(deal_new_stack(deck)) == [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+
+def test_cut():
+    deck = np.arange(10)
+    assert list(cut_stack(deck, 3)) == [3, 4, 5, 6, 7, 8, 9, 0, 1, 2]
+    assert list(cut_stack(deck, -4)) == [6, 7, 8, 9, 0, 1, 2, 3, 4, 5]
+
+def test_deal_with_increment():
+    deck = np.arange(10)
+    assert list(deal_with_increment(deck, 3)) == [0, 7, 4, 1, 8, 5, 2, 9, 6, 3]
+
+EXAMPLES = [
+    (
+        [
+            "deal with increment 7",
+            "deal into new stack",
+            "deal into new stack",
+        ],
+        [0, 3, 6, 9, 2, 5, 8, 1, 4, 7],
+    ),
+    (
+        [
+            "cut 6",
+            "deal with increment 7",
+            "deal into new stack",
+        ],
+        [3, 0, 7, 4, 1, 8, 5, 2, 9, 6],
+    ),
+    (
+        [
+            "deal with increment 7",
+            "deal with increment 9",
+            "cut -2",
+        ],
+        [6, 3, 0, 7, 4, 1, 8, 5, 2, 9],
+    ),
+    (
+        [
+            "deal into new stack",
+            "cut -2",
+            "deal with increment 7",
+            "cut 8",
+            "cut -4",
+            "deal with increment 7",
+            "cut 3",
+            "deal with increment 9",
+            "deal with increment 3",
+            "cut -1",
+        ],
+        [9, 2, 5, 8, 1, 4, 7, 0, 3, 6],
+    ),
+]
+
+@pytest.mark.parametrize("techniques,result", EXAMPLES)
+def test_follow_card(techniques, result):
+    commands = parse_techniques(techniques)
+    for i in range(10):
+        assert follow_card(commands, i, 10) == result.index(i)
+
+@pytest.mark.parametrize("techniques,result", EXAMPLES)
+def test_revert_card(techniques, result):
+    commands = parse_techniques(techniques)
+    for i in range(10):
+        assert revert_card(commands, i, 10) == result[i]
+
+def test_merge_commands():
+    commands = parse_techniques(EXAMPLES[-1][0])
+    assert merge_commands(commands, 10) == [(2, 7), (1, 3)]
+
+def test_merge_commands_n_times():
+    commands = parse_techniques(EXAMPLES[-1][0])
+    for i in range(10):
+        expected = merge_commands(commands * i, 10)
+        assert merge_commands_n_times(commands, 10, i) == expected
