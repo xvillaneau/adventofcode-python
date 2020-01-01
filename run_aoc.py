c2103a01d3106f708753ad823c96f44e6328697d
Xavier Villaneau <xvillaneau@gmail.com>
2019-12-31 19:33:39 -0500

    Added support for only having a "main" generator

--- run_aoc.py
+++ run_aoc.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import argparse
+from functools import partial
 from importlib import import_module
 from importlib.util import spec_from_file_location, module_from_spec
 from pathlib import Path
 import sys
+from time import perf_counter_ns
 
 if sys.version_info < (3, 8):
     raise RuntimeError("Only Python >= 3.8 is supported")
@@ -12,14 +14,11 @@ YEARS = [2019]  # Add more over time
 AOC_ROOT = Path(__file__).parent
 
 
-def ask_or_abort(message: str):
-    answer = input(message + " [y/N] ")
-    if answer.lower() not in ["yes", "y", "true"]:
-        print("Aborting")
-        sys.exit(1)
-
-
 def load_year(year: int):
+    """
+    Load the package with the year's code. Designed to work without
+    having to install the code in the PYTHONPATH.
+    """
     name = f"aoc_{year}"
     if name in sys.modules:
         return
@@ -35,19 +34,64 @@ def load_year(year: int):
     spec.loader.exec_module(module)
 
 
-def get_runner(year: int, day: int):
+def get_main(year: int, day: int):
+    """
+    Load the module for the given day (assuming the year's package was
+    loaded with load_year), and look for a suitable main.
+    """
     module = import_module(f"aoc_{year}.day_{day:02}")
 
-    if not hasattr(module, "AocRunner"):
-        raise ImportError(f"AocRunner not found in {module}")
+    if hasattr(module, "main"):
+        # Has a main function, expected to be a generator that takes
+        # the full input file as argument.
+        return partial(run_day_main, year, day, module.main)
 
-    return module.AocRunner()
+    if hasattr(module, "AocRunner"):
+        # Has an AocRunner class. This was a bad idea.
+        return module.AocRunner().aoc_main
+
+    raise ImportError(f"No compatible main found in {module}")
 
 
-def main(year: int, day: int, yes: bool):
+def timed_next(generator):
+    """Get the next value from a generator and time how long it takes"""
+    start = perf_counter_ns()
+    result = next(generator)
+    stop = perf_counter_ns()
+    return result, (stop - start)
+
+
+def run_day_main(year: int, day: int, day_main):
+    print(f"Advent of Code year {year}, day {day}")
+
+    data_path = AOC_ROOT / f"aoc_{year}" / "data" / f"day_{day:02}.txt"
+    with open(data_path, "r") as file:
+        main = day_main(file.read())
+
+    p1_res, p1_time = timed_next(main)
+    print("Part 1:", p1_res)
+    print(f"\tRan in {p1_time:,} ns")
+
+    try:
+        p2_res, p2_time = timed_next(main)
+        print("Part 2:", p2_res)
+        print(f"\tRan in {p2_time:,} ns")
+    except StopIteration:
+        pass
+
+
+def process_args(year: int, day: int, interactive: bool):
+    """Select the years/days to run based on the input"""
+
+    def ask_or_abort(message: str):
+        answer = input(message + " [y/N] ")
+        if answer.lower() not in ["yes", "y", "true"]:
+            print("Aborting")
+            sys.exit(1)
+
     # Process the year input
     if year == -1:
-        if not yes:
+        if interactive:
             ask_or_abort("This will run ALL the AoC solutions. Are you really sure?")
         years = YEARS
     else:
@@ -56,12 +100,18 @@ def main(year: int, day: int, yes: bool):
 
     # Process the day input
     if day == -1:
-        if year != -1 and not yes:
+        if year != -1 and interactive:
             ask_or_abort(f"This will run all solutions for AoC {year}, are you sure?")
         days = list(range(1, 26))
     else:
         assert 1 <= day <= 25
         days = [day]
+
+    return years, days
+
+
+def aoc_main(year: int, day: int, interactive: bool = True):
+    years, days = process_args(year, day, interactive)
 
     # Run the solutions!
     for year in years:
@@ -70,17 +120,17 @@ def main(year: int, day: int, yes: bool):
 
         for day in days:
             try:
-                runner = get_runner(year, day)
+                day_main = get_main(year, day)
             except ImportError:
                 print(f"Skipping {year} day {day}, import failed")
             else:
-                runner.main()
+                day_main()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("year", type=int, nargs="?", default=-1)
     parser.add_argument("day", type=int, nargs="?", default=-1)
-    parser.add_argument("--yes", "-y", action="store_true")
+    parser.add_argument("--yes", "-y", dest="interactive", action="store_false")
 
-    main(**vars(parser.parse_args()))
+    aoc_main(**vars(parser.parse_args()))
