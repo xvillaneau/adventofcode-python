f0690e3dfc0bdacc3bd30fcb6d560ac2bf5ef240
Xavier Villaneau <xvillaneau@gmail.com>
2020-01-03 12:40:45 -0500

    Rewrote 2019.04

--- aoc_2019/src/aoc_2019/day_04.py
+++ aoc_2019/src/aoc_2019/day_04.py
@@ -1,36 +1,55 @@
-import re
+def part_1(start: int, stop: int):
+    assert 100_000 <= start <= 999_999
+    assert 100_000 <= stop <= 999_999
 
-RE_PART_1 = re.compile(r"(.)\1")
-RE_PART_2 = re.compile(r"(.)(?<!\1.)\1(?!\1)")
+    start_top, stop_top = start // 100_000, stop // 100_000
+
+    def _password_gen(number: int, magnitude: int, last_digit: int, has_double: bool):
+        if magnitude == 100_000:
+            for digit in range(start_top, min(stop_top, last_digit) + 1):
+                num = number + digit * magnitude
+                if (has_double or digit == last_digit) and start <= num <= stop:
+                    yield num
+        else:
+            next_magnitude = magnitude * 10
+            for digit in range(last_digit + 1):
+                num = number + digit * magnitude
+                double = has_double or digit == last_digit
+                yield from _password_gen(num, next_magnitude, digit, double)
+
+    return sum(True for i in range(10) for _ in _password_gen(i, 10, i, False))
 
 
-def acceptable_passwords(start: int, end: int, re_check):
+def part_2(start: int, stop: int):
+    assert 100_000 <= start <= 999_999
+    assert 100_000 <= stop <= 999_999
 
-    start = tuple(map(int, str(start)))
-    end = tuple(map(int, str(end)))
-    assert len(start) == len(end)
-    depth = len(start)
+    start_top, stop_top = start // 100_000, stop // 100_000
 
-    def _rec_passwords(stack):
-        if len(stack) == depth:
-            full_pw = "".join(map(str, stack))
-            yield bool(re_check.search(full_pw))
-            return
-        prev = stack[-1] if stack else 0
-        zeros = (0,) * (depth - len(stack) - 1)
-        nines = (9,) * len(zeros)
-        for digit in range(prev, 10):
-            _nxt = stack + (digit,)
-            if _nxt + nines < start:
-                continue
-            if _nxt + zeros > end:
-                break
-            yield from _rec_passwords(_nxt)
+    def _password_gen(
+        number: int, magnitude: int, last_digit: int, streak: int, has_good_double: bool
+    ):
+        if magnitude == 100_000:
+            for digit in range(start_top, min(stop_top, last_digit) + 1):
+                num = number + digit * magnitude
+                repeat = 1 if digit != last_digit else streak + 1
+                if start <= num <= stop and (
+                    has_good_double or repeat == 2 or repeat == 1 and streak == 2
+                ):
+                    yield num
 
-    yield from _rec_passwords(())
+        else:
+            next_magnitude = magnitude * 10
+            for digit in range(last_digit + 1):
+                num = number + digit * magnitude
+                repeat = 1 if digit != last_digit else streak + 1
+                good = has_good_double or (repeat == 1 and streak == 2)
+                yield from _password_gen(num, next_magnitude, digit, repeat, good)
+
+    return sum(True for i in range(10) for _ in _password_gen(i, 10, i, 1, False))
 
 
 def main(data: str):
     start, stop = map(int, data.strip().split("-"))
-    yield sum(acceptable_passwords(start, stop, RE_PART_1))
-    yield sum(acceptable_passwords(start, stop, RE_PART_2))
+    yield part_1(start, stop)
+    yield part_2(start, stop)
