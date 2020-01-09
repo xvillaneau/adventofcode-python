5ef1a43202c6cd496cc2bb034f49405940d20e4d
Xavier Villaneau <xvillaneau@gmail.com>
2020-01-09 12:45:03 -0500

    Made 2018.04 much faster

--- aoc_2018/src/aoc_2018/day_04.py
+++ aoc_2018/src/aoc_2018/day_04.py
@@ -1,22 +1,21 @@
-from datetime import datetime
 from heapq import heappush, heappop
 from typing import Tuple
 
 import numpy as np
 
-
-Event = Tuple[datetime, int]
+Event = Tuple[str, int, int]
 
 
 def parse_line(line: str) -> Event:
-    timestamp = datetime.strptime(line[1:17], '%Y-%m-%d %H:%M')
+    timestamp = line[1:14]
+    minute = int(line[15:17])
     message = line[19:]
     if message == 'wakes up':
-        return timestamp, -1
+        return timestamp, minute, -1
     elif message == "falls asleep":
-        return timestamp, -2
+        return timestamp, minute, -2
     else:
-        return timestamp, int(message[7:].partition(" ")[0])
+        return timestamp, minute, int(message[7:].partition(" ")[0])
 
 
 def parse_schedule(data: str):
@@ -26,7 +25,7 @@ def parse_schedule(data: str):
         event = parse_line(line)
         heappush(events, event)
 
-        if (n := event[1]) < 0 or n in guard_ids:
+        if (n := event[2]) < 0 or n in guard_ids:
             continue
         guard_ids[n] = len(guards)
         guards.append(n)
@@ -35,11 +34,11 @@ def parse_schedule(data: str):
 
     line_num = len(guards)
     while events:
-        timestamp, event = heappop(events)
+        _, minute, event = heappop(events)
         if event >= 0:
             line_num = guard_ids[event]
         else:
-            schedule[line_num, timestamp.minute:] += (1 if event == -2 else -1)
+            schedule[line_num, minute:] += (1 if event == -2 else -1)
 
     return guards, schedule
 
@@ -48,5 +47,6 @@ def main(data: str):
     guards, schedule = parse_schedule(data)
     most_asleep = int(np.argmax(np.sum(schedule, axis=1)))
     yield guards[most_asleep] * np.argmax(schedule[most_asleep])
+
     most_total_line, most_total_min = divmod(schedule.argmax(), 60)
     yield guards[most_total_line] * most_total_min
