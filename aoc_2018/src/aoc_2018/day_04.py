fca41df52e25cfc65d80003f79f103ccbdf7e475
Xavier Villaneau <xvillaneau@gmail.com>
2020-01-09 12:24:37 -0500

    Improved 2018.04

--- aoc_2018/src/aoc_2018/day_04.py
+++ aoc_2018/src/aoc_2018/day_04.py
@@ -1,58 +1,52 @@
-
-from dataclasses import dataclass
 from datetime import datetime
-import re
-from typing import Tuple, Union
+from heapq import heappush, heappop
+from typing import Tuple
 
 import numpy as np
 
-RE_LINE = re.compile(r'\[(.*)\] (.*)')
-RE_GUARD = re.compile(r'Guard #(\d+) begins shift')
+
+Event = Tuple[datetime, int]
 
 
-@dataclass
-class Guard:
-    id: int
-
-
-@dataclass
-class Event:
-    minute: int
-    asleep: bool
-
-
-def parse_line(line: str) -> Tuple[datetime, Union[Guard, Event]]:
-    date_s, message = RE_LINE.search(line).groups()
-    timestamp = datetime.strptime(date_s, '%Y-%m-%d %H:%M')
+def parse_line(line: str) -> Event:
+    timestamp = datetime.strptime(line[1:17], '%Y-%m-%d %H:%M')
+    message = line[19:]
     if message == 'wakes up':
-        event = Event(timestamp.minute, False)
+        return timestamp, -1
     elif message == "falls asleep":
-        event = Event(timestamp.minute, True)
+        return timestamp, -2
     else:
-        event = Guard(int(RE_GUARD.search(message).groups()[0]))
-    return timestamp, event
+        return timestamp, int(message[7:].partition(" ")[0])
 
 
-def parse_schedule(lines):
-    events = [parse_line(l) for l in lines]
-    guards = list(sorted(set(g.id for _, g in events if isinstance(g, Guard))))
+def parse_schedule(data: str):
 
-    asleep_times = np.zeros((len(guards), 61), dtype=int)
-    asleep_times[:, 0] = guards
+    events, guards, guard_ids = [], [], {}
+    for line in data.splitlines():
+        event = parse_line(line)
+        heappush(events, event)
 
-    line_num = 0
-    sorted_events = (e for _, e in sorted(events, key=lambda t: t[0]))
-    for event in sorted_events:
-        if isinstance(event, Guard):
-            line_num = guards.index(event.id)
+        if (n := event[1]) < 0 or n in guard_ids:
+            continue
+        guard_ids[n] = len(guards)
+        guards.append(n)
+
+    schedule = np.zeros((len(guards), 60), dtype=int)
+
+    line_num = len(guards)
+    while events:
+        timestamp, event = heappop(events)
+        if event >= 0:
+            line_num = guard_ids[event]
         else:
-            asleep_times[line_num, event.minute + 1:] += (1 if event.asleep else -1)
-    return asleep_times
+            schedule[line_num, timestamp.minute:] += (1 if event == -2 else -1)
+
+    return guards, schedule
 
 
-def main(data):
-    schedule = parse_schedule(data.splitlines())
-    most_times = max(schedule, key=lambda l: l[1:].sum())
-    most_total_line, most_total_min = divmod(schedule[:, 1:].argmax(), 60)
-    yield most_times[0] * most_times[1:].argmax()
-    yield schedule[most_total_line, 0] * most_total_min
+def main(data: str):
+    guards, schedule = parse_schedule(data)
+    most_asleep = int(np.argmax(np.sum(schedule, axis=1)))
+    yield guards[most_asleep] * np.argmax(schedule[most_asleep])
+    most_total_line, most_total_min = divmod(schedule.argmax(), 60)
+    yield guards[most_total_line] * most_total_min
