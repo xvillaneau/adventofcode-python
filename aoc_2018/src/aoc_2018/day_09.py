c7b1f1a44f74d0dbb8969b7c60e7118dd7f3e3cb
Xavier Villaneau <xvillaneau@gmail.com>
2020-01-10 14:16:05 -0500

    Small formatting fixes on 2018.09

--- aoc_2018/src/aoc_2018/day_09.py
+++ aoc_2018/src/aoc_2018/day_09.py
@@ -1,12 +1,8 @@
-
 from collections import deque
 import re
 
 
 class Game:
-
-    __slots__ = ['marbles', 'scores', 'current_value', 'next_scorer']
-
     def __init__(self, n_players: int):
         self.marbles = deque([0])
         self.scores = [0] * n_players
@@ -18,7 +14,9 @@ class Game:
             self.marbles.rotate(-2)
             self.marbles.appendleft(self.current_value + i)
         self.marbles.rotate(7)
-        self.scores[self.next_scorer] += self.marbles.popleft() + self.current_value + 22
+        self.scores[self.next_scorer] += (
+            self.marbles.popleft() + self.current_value + 22
+        )
         self.next_scorer = (self.next_scorer + 23) % len(self.scores)
         self.current_value += 23
 
@@ -29,9 +27,9 @@ class Game:
 
 def main(game_input: str):
     # Parse the puzzle input
-    match = re.search(r'(\d+) players; last marble is worth (\d+) points', game_input)
-    players, max_val = map(int, match.groups())
-    # Run game for first rounf
+    match = re.search(r"(\d+) players; last marble is worth (\d+) points", game_input)
+    players, max_val = int(match.group(1)), int(match.group(2))
+    # Run game for first round
     g = Game(players)
     g.play(max_val)
     yield max(g.scores)
