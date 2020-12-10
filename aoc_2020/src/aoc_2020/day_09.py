8e08825a9854fbc619bf7337ac51b7ece54345f4
Xavier Villaneau <xvillaneau@gmail.com>
2020-12-10 09:55:04 -0500

    Optimize AOC 2020 day 9

--- aoc_2020/src/aoc_2020/day_09.py
+++ aoc_2020/src/aoc_2020/day_09.py
@@ -1,6 +1,8 @@
 from collections import defaultdict
 from typing import List
 
+from more_itertools import windowed
+
 
 def find_invalid(message: List[int], buffer=25) -> int:
     tail = 0
@@ -12,26 +14,35 @@ def find_invalid(message: List[int], buffer=25) -> int:
                 return num
 
             removed = message[tail]
-            for i in range(tail + 1, head):
-                sums[removed + message[i]] -= 1
+            for n in message[tail + 1:head]:
+                sums[removed + n] -= 1
             tail += 1
 
-        for i in range(tail, head):
-            sums[num + message[i]] += 1
+        for n in message[tail:head]:
+            sums[num + n] += 1
 
     return -1
 
 
 def find_weakness(message: List[int], invalid_num: int) -> int:
-    prev_sums = []
-    for i, num in enumerate(message):
-        for j, p_sum in enumerate(prev_sums):
-            if num + p_sum == invalid_num:
-                num_range = message[j:i + 1]
-                return min(num_range) + max(num_range)
-            prev_sums[j] += num
-        prev_sums.append(num)
-    return -1
+
+    def _weakness(size, start, end):
+        if end - start < size:
+            return None
+
+        windows = windowed(message[start:end], size)
+        for k, window in enumerate(windows, start=start):
+            window_sum = sum(window)
+            if window_sum == invalid_num:
+                return min(window) + max(window)
+            elif window_sum > invalid_num:
+                if (res := _weakness(size + 1, start, k + size - 1)):
+                    return res
+                start = k + 1
+
+        return _weakness(size + 1, start, end)
+
+    return _weakness(2, 0, len(message))
 
 
 def main(data: str):
